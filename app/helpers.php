86673b09b7d38d9b3965c14f77e95c4740b18146
RolexxTime <87321298+RolexxTime@users.noreply.github.com>
2022-05-06 14:11:24 +0200

    Add page_title helper

---
+++ app/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+if(! function_exists('page_title')){
+    
+    function page_title($title){
+        
+        $base_title = 'Laracarte - Lists of Artisans';
+
+        if($title === ''){
+
+            return $base_title;
+
+        }else{
+
+            return $title. ' | ' . $base_title;
+
+        }
+    }
+}
